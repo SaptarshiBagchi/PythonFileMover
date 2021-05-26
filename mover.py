bb4821b6737ca7db30701e2a050feaf565d44a39
SaptarshiBagchi <55134979+SaptarshiBagchi@users.noreply.github.com>
2021-05-26 19:35:15 +0530

    File created
    File done

---
+++ mover.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+import pandas as pd
+
+data = pd.read_excel('ABL_Test.xlsx')
+df = pd.DataFrame(data, columns=['File Name'])
+df = df['File Name'].values.tolist()
+print(df)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+for root, dirs, files in os.walk(dir_path):
+    for file in files:
+        if str(file) in df:
+            # os.rename(root + '' + str(file),
+            #           root + '/moved/' + str(file))
+            shutil.copy(file, 'moved')
